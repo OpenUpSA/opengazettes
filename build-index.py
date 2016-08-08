96afdfdc11297744384695ec6434c398bded6edd
Greg Kempe <greg@kempe.net>
2016-08-08 10:21:27 +0200

    Tweak data layout

--- build-index.py
+++ build-index.py
@@ -23,7 +23,7 @@ def write_year(juri, year, gazettes):
         f.write("jurisdiction: %s\n" % juri)
         f.write("jurisdiction_name: %s\n" % jurisdictions[juri])
         f.write("provincial: %s\n" % str(juri != "ZA").lower())
-        f.write("year: \"%s\"\n" % year)
+        f.write('year: "%s"\n' % year)
         f.write("---\n")
 
 
@@ -54,23 +54,30 @@ def write_jurisdiction(juri, years):
 
 
 def build_index():
-    for juri in jurisdictions.iterkeys():
-        gazettes[juri] = defaultdict(list)
+    for juri, name in jurisdictions.iteritems():
+        gazettes[juri] = {
+            'name': name,
+            'years': set(),
+            'gazettes': defaultdict(list)
+        }
 
     for line in open('gazette-index-latest.jsonlines'):
         gazette = json.loads(line)
         juri = gazette['jurisdiction_code']
-        year = gazette['publication_date'].split('-')[0]
+        year = int(gazette['publication_date'].split('-')[0])
         if 'archive_url' not in gazette:
             gazette['archive_url'] = 'http://code4sa-gazettes.s3.amazonaws.com/archive/' + gazette['archive_path']
-        gazettes[juri][year].append(gazette)
+
+        gazettes[juri]['gazettes'][year].append(gazette)
+        gazettes[juri]['years'].add(year)
 
     for juri in gazettes.iterkeys():
-        write_jurisdiction(juri, gazettes[juri])
+        write_jurisdiction(juri, gazettes[juri]['gazettes'])
 
     # sort gazettes by date
     for juris in gazettes.itervalues():
-        for items in juris.itervalues():
+        juris['years'] = sorted(list(juris['years']))
+        for items in juris['gazettes'].itervalues():
             items.sort(key=lambda g: g['publication_date'])
 
     with open('_data/gazettes.json', 'w') as f:
