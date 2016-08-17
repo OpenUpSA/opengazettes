512004f0a08df7d17d89cf94d14b1727c449a4fa
Greg Kempe <greg@kempe.net>
2016-08-17 15:07:52 +0200

    Travis (#8)
    * Support for pushing to gh-pages from travis
    
    * Install jekyll
    
    * Use ruby buildpack on travis, that's what we need deps for
    
    * Use ruby
    
    * Use rvm
    
    * Exclude vendor directory for travis
    
    * README

---
+++ bin/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "Deploying to GitHub"
+
+# add git auth
+eval "$(ssh-agent -s)" #start the ssh agent
+chmod 600 deploy_key # this key should have push access
+ssh-add deploy_key
+
+git config user.email "<webapps@code4sa.org>"
+git config user.name "Open Gazettes (via TravisCI)"
+git add _data _gazettes
+git commit -m "Updated index via TravisCI [ci skip]"
+git remote set-url origin git@github.com:code4sa/opengazettes.git
+git push
