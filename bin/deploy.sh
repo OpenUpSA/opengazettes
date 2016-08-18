f0b3770b9af8ab31447f3440e214b395c2368c37
Greg Kempe <greg@kempe.net>
2016-08-18 13:33:32 +0200

    Checkout travis branch during deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -10,6 +10,7 @@ ssh-add deploy_key
 
 git config user.email "<webapps@code4sa.org>"
 git config user.name "Open Gazettes (via TravisCI)"
+git checkout ${TRAVIS_BRANCH}
 git add _data _gazettes
 git commit -m "Updated index via TravisCI [ci skip]"
 git remote set-url origin git@github.com:code4sa/opengazettes.git
