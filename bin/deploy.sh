d1b765d621c692ef2a4bc199f49cb39208adec79
Greg Kempe <greg@kempe.net>
2016-09-01 10:03:39 +0200

    master -> gh-pages

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,12 +2,12 @@
 set -e -x
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "build" ]; then
-  # switch to master branch
+  # switch to gh-pages branch
   git config user.email "<webapps@code4sa.org>"
   git config user.name "Open Gazettes (via TravisCI)"
-  git fetch origin master
+  git fetch origin gh-pages
   git checkout FETCH_HEAD
-  git checkout -b master
+  git checkout -b gh-pages
 
   # update the index file and rebuild site data
   curl http://code4sa-gazettes.s3.amazonaws.com/archive/index/gazette-index-latest.jsonlines -O
@@ -21,7 +21,7 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "build" ]; t
   git add _data _gazettes
   git commit -m "Updated index via TravisCI"
 
-  # now update master branch on github
+  # now update gh-pages branch on github
   echo "Deploying to GitHub"
 
   # add git auth
@@ -31,7 +31,7 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "build" ]; t
   ssh-add deploy_key
 
   git remote set-url origin git@github.com:code4sa/opengazettes.git
-  git push origin -u master
+  git push origin -u gh-pages
 else
   echo "Ignoring pull request or non-build branch"
   exit 0
