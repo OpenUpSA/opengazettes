35c6c3324bd70dcc7a88bfbdeaf29fe3b95d9933
Greg Kempe <greg@kempe.net>
2016-09-01 08:54:16 +0200

    Updated build script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 set -e
 
-echo "Deploying to GitHub"
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "build" ]; then
+  # switch to master branch
+  git config user.email "<webapps@code4sa.org>"
+  git config user.name "Open Gazettes (via TravisCI)"
+  git checkout master
 
-# add git auth
-eval "$(ssh-agent -s)" #start the ssh agent
-chmod 600 deploy_key # this key should have push access
-ssh-add deploy_key
+  # update the index file and rebuild site data
+  curl http://code4sa-gazettes.s3.amazonaws.com/archive/index/gazette-index-latest.jsonlines -O
+  python bin/build-index.py
 
-git config user.email "<webapps@code4sa.org>"
-git config user.name "Open Gazettes (via TravisCI)"
-git checkout ${TRAVIS_BRANCH}
-git add _data _gazettes
-git commit -m "Updated index via TravisCI [ci skip]"
-git remote set-url origin git@github.com:code4sa/opengazettes.git
-git push
+  # ensure the site builds
+  bundle install
+  bundle exec jekyll build
+
+  # save changes
+  git add _data _gazettes
+  git commit -m "Updated index via TravisCI"
+
+  # now update master branch on github
+  echo "Deploying to GitHub"
+
+  # add git auth
+  eval "$(ssh-agent -s)" #start the ssh agent
+  openssl aes-256-cbc -K $encrypted_152ca5bd4b01_key -iv $encrypted_152ca5bd4b01_iv -in deploy_key.enc -out deploy_key -d && bin/build-index.sh
+  chmod 600 deploy_key # this key should have push access
+  ssh-add deploy_key
+
+  git remote set-url origin git@github.com:code4sa/opengazettes.git
+  git push
+else
+  echo "Ignoring pull request or non-build branch"
+  exit 0
+fi
