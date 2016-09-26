33af147de835cace51c15f8be3eb38cdd6ea0d67
smmbll <lion@smmbll.com>
2016-09-26 16:17:12 +0200

    Synced up link colors

--- js/main.js
+++ js/main.js
@@ -3,4 +3,8 @@ $(function() {
   $('a[href^=http]').on('click', function(e) {
     ga('send', 'event', 'outbound-click', e.target.href);
   });
+
+  var url = window.location.href.split('/');
+
+  $('#' + url[url.length - 1]).addClass('active');
 });
